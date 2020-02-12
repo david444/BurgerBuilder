c890d259c7d604578687931868d7845c4ec5eaad
Mac <davidain03@gmail.com>
2020-02-12 14:36:05 -0700

    Lesson 181 Drawer bar with toggle options

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,17 +5,24 @@ import Toolbar from '../Navigation/Toolbar/Toolbar.js';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 class Layout extends Component {
-    state ={
-        showSideDrawer:true
+    state = {
+        showSideDrawer: false
     }
 
     sideDrawerClosedHandler = () => {
-        this.setState({showSideDrawer:false})
+        this.setState({ showSideDrawer: false })
     }
+
+    sideDrawerdHandler = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
     render() {
         return (
             <Aux>
-                <Toolbar />
+                <Toolbar drawerToggleClicked={this.sideDrawerdHandler} />
                 <SideDrawer open={this.state.showSideDrawer} closed={this.sideDrawerClosedHandler} />
                 <main className={classes.Content}>
                     {this.props.children}

a6cf332867cacf00921851aef3c210e5b409584b
Mac <davidain03@gmail.com>
2020-02-17 09:07:31 -0700

    Lesson 185: app performance improvement

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import Aux from '../../../hoc/Auxiliary';
 import Button from '../../UI/Button/Button';
 
-const orderSummary = (props) => {
-    const ingridientSummary = Object.keys(props.ingridients)
+class OrderSummary extends Component{
+    //this could be a functional component.
+    componentWillUpdate(){
+        console.log('[Order summary]: will update')
+    }
+
+    render() {
+        const ingridientSummary = Object.keys(this.props.ingridients)
         .map(igKey => {
             return (
             <li key={igKey}>
-                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {props.ingridients[igKey]}
+                <span style={{ textTransform: 'capitalize' }}>{igKey}</span>: {this.props.ingridients[igKey]}
             </li>);
         });
-    return (
-        <Aux>
+        return(
+            <Aux>
             <h3>Your order</h3>
             <p>A delicious burger with the following ingridients:</p>
             <ul>
                 {ingridientSummary}
             </ul>
-            <p><strong> Total Price: {props.price.toFixed(2)}</strong></p>
+            <p><strong> Total Price: {this.props.price.toFixed(2)}</strong></p>
             <p>Continue to Checkout?</p>
-            <Button btnType="Danger" clicked={props.purchaseCancel}>CANCEL</Button>
-            <Button btnType="Success" clicked={props.purchaseContinue}>CONTINUE</Button>
+            <Button btnType="Danger" clicked={this.props.purchaseCancel}>CANCEL</Button>
+            <Button btnType="Success" clicked={this.props.purchaseContinue}>CONTINUE</Button>
         </Aux>
-    )
+        )
+    }
+    
 };
 
-export default orderSummary;
+export default OrderSummary;

eb46dda9eb351f910260dbf4f1783961db401960
Mac <davidain03@gmail.com>
2020-03-02 10:50:30 -0700

    Lecture 213: adding firebase to projecto for post information with axios, and handling errors to it

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -4,6 +4,9 @@ import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGRIDIENT_PRICES = {
     salad: 0.5,
@@ -25,7 +28,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchaseable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState(ingridients) {
@@ -77,8 +81,30 @@ class BurgerBuilder extends Component {
         this.setState({ purchasing: false });
     }
 
-    purchaseContinueHandler = ()=>{
-        alert('You continue!');
+    purchaseContinueHandler = () => {
+        // alert('You continue!');
+        this.setState({loading:true});
+        const order = {
+            ingridients: this.state.ingridients,
+            price: this.state.totalPrice, //in production this would be calculated in the server, so the user don't manipulate the price
+            customer: {
+                name: 'Pollo',
+                address: {
+                    street: 'Test 1',
+                    zipCode: '1512',
+                    country: 'Germany'
+                },
+                email: 'test@dummy.com',
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders', order)
+            .then(response => {
+                this.setState({loading:false, purchasing:false});
+            })
+            .catch(error => {
+                this.setState({loading:false, purchasing:false});
+            });
     }
     render() {
         const disabledInfo = {
@@ -87,15 +113,19 @@ class BurgerBuilder extends Component {
         for (const key in disabledInfo) {
             disabledInfo[key] = disabledInfo[key] <= 0;
         }
+        let orderSummary = <OrderSummary
+                ingridients={this.state.ingridients}
+                purchaseCancel={this.purchaseCancelHandler}
+                purchaseContinue={this.purchaseContinueHandler}
+                price={this.state.totalPrice}
+            />
+        if (this.state.loading) {
+            orderSummary = <Spinner />
+        }
         return (
             <Aux>
                 <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingridients={this.state.ingridients}
-                        purchaseCancel={this.purchaseCancelHandler}
-                        purchaseContinue={this.purchaseContinueHandler}
-                        price={this.state.totalPrice}
-                    />
+                    {orderSummary}
                 </Modal>
                 <Burger ingridients={this.state.ingridients} />
                 <BuildControls
@@ -111,4 +141,4 @@ class BurgerBuilder extends Component {
     }
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
